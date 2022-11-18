e42a67f962f35c09000062de2fbecbdf6a03383b
Charlie Whight <36269225+cwhight@users.noreply.github.com>
2022-11-18 21:00:25 +0700

    Update match.go

--- internal/db/match.go
+++ internal/db/match.go
@@ -12,23 +12,28 @@ func NewMatchDb() MatchDb {
 }
 
 func (d *MatchDb) SaveSwipe(swipe *model.Swipe) error {
+	// store given swipe in swipe table
 	return nil
 }
 
 func (d *MatchDb) GetSwipes(userId uuid.UUID) (map[uuid.UUID]bool, error) {
+	// fetch all 'swipes' for given user id - return map[id]bool with all profile ids (i.e users that the userId has swiped for previously)
 	return map[uuid.UUID]bool{uuid.New(): true}, nil
 }
 
 func (d *MatchDb) CheckMatch(swipe *model.Swipe) (*model.Match, error) {
+	// returns false match if current user swiped 'NO'
 	if swipe.Preference == "NO" {
 		return &model.Match{Matched: false}, nil	
 	}
 
+	// checks for match
 	matched, err :=  d.getMatch(swipe.UserId, swipe.ProfileId)
 	if err != nil {
 		return nil, err
 	}
 
+	// returns successful match and gens match ID if matching swipe exists
 	if matched {
 		return &model.Match{Matched: matched, MatchId: uuid.New()}, nil
 	} 
@@ -37,5 +42,8 @@ func (d *MatchDb) CheckMatch(swipe *model.Swipe) (*model.Match, error) {
 }
 
 func (d *MatchDb) getMatch(userId uuid.UUID, profileId uuid.UUID) (bool, error) {
+	// checks if 'swipe' exists for reverse of userId and profileId
+	// i.e other user has swiped the current user
+	// return true if has swiped and has macthed
 	return true, nil
 }
